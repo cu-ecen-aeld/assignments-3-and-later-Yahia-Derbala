194f6fc424dee6a673d682a4e788e146a949234a
Yahia-Derbala <yahderbala@gmail.com>
2023-11-27 13:54:39 +0200

    Fixing start script

--- server/aesdsocket-start-stop.sh
+++ server/aesdsocket-start-stop.sh
@@ -1,16 +1,23 @@
-#!/bin/bash
+#!/bin/sh
+
+SERVICE_NAME=aesdsocket
+SERVICE_PATH=/usr/bin/aesdsocket
+SERVICE_ARGS=-d
+
 
 case "$1" in
     start)
-        echo "Starting aesdsocket daemon..."
-        start-stop-daemon --start --background --pidfile /var/run/aesdsocket.pid --make-pidfile --exec /usr/bin/aesdsocket -- -d
+        echo "Starting aesdocket daemon"
+        start-stop-daemon -S -n ${SERVICE_NAME} -a "${SERVICE_PATH}" -- ${SERVICE_ARGS}
         ;;
+
     stop)
-        echo "Stopping aesdsocket daemon..."
-        start-stop-daemon --stop --pidfile /var/run/aesdsocket.pid
+        echo "Stopping aesdocket daemon"
+        # defaults to SIGTERM
+        start-stop-daemon -K -n ${SERVICE_NAME}
         ;;
     *)
-        echo "Usage: $0 {start|stop}"
+        echo "Usage: $0 start|stop"
         exit 1
         ;;
 esac
