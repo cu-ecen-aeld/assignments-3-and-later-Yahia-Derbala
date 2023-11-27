b592b0a9a1f7868af2a6031c21a1be04bd03fb5f
Yahia-Derbala <yahderbala@gmail.com>
2023-11-27 14:37:43 +0200

    fixing stop start

--- server/aesdsocket-start-stop.sh
+++ server/aesdsocket-start-stop.sh
@@ -1,25 +1,17 @@
 #!/bin/sh
 
-SERVICE_NAME=aesdsocket
-SERVICE_PATH=usr/bin/aesdsocket
-SERVICE_ARGS=-d
-
-
 case "$1" in
-    start)
-        echo "Starting aesdocket daemon"
-        start-stop-daemon -S -n ${SERVICE_NAME} -a "${SERVICE_PATH}" -- ${SERVICE_ARGS}
-        ;;
-
-    stop)
-        echo "Stopping aesdocket daemon"
-        # defaults to SIGTERM
-        start-stop-daemon -K -n ${SERVICE_NAME}
-        ;;
-    *)
-        echo "Usage: $0 start|stop"
-        exit 1
-        ;;
+	start)
+		echo "Starting aesdsocket"
+		start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket -- -d
+		;;
+	stop)
+		echo "Stopping aesdsocket"
+		start-stop-daemon -K -n aesdsocket
+		;;
+	*)
+		echo "Usage $0 (start|stop)"
+	exit 1
 esac
 
 exit 0
