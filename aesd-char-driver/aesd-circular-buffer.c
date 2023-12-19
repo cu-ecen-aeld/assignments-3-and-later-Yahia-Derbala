3037c990f9b82b500450c1b1dd72b95ccd510f07
Yahia-Derbala <yahderbala@gmail.com>
2023-12-19 11:27:28 +0200

    Assignment 8 Complete

--- aesd-char-driver/aesd-circular-buffer.c
+++ aesd-char-driver/aesd-circular-buffer.c
@@ -32,9 +32,9 @@ struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct
     /**
     * TODO: implement per description
     */
- size_t cumulative_offset = 0;
+    size_t cumulative_offset = 0;
 
-    for (uint8_t i = buffer->out_offs, i = 0; i < AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED; ++i, i = (i + 1) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED) {
+    for (uint8_t i = buffer->out_offs; i < AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED; i = (i + 1) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED) {
         // Check if the buffer is not full and the current index is equal to in_offs
         if (!(buffer->full) && i == buffer->in_offs) {
             break; // Exit loop if in_offs is encountered without finding the offset
